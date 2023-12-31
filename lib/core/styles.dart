41350c3f284a66b3488b8c0f56440f34a8d2ff72
Mohammed_Elsheikh <moh.reda198@gmail.com>
2023-12-31 22:37:19 +0200

    Create set up of app.

---
+++ lib/core/styles.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const textStyle25 = TextStyle(
+    fontSize: 25,
+    fontWeight: FontWeight.w600,
+  );
+  static const textStyle40 = TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.w600,
+  );
+  static const textStyle90 = TextStyle(
+    fontSize: 90,
+    fontWeight: FontWeight.w600,
+  );
+}
