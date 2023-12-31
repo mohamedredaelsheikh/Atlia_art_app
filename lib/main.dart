9d63388f834804647175fb693df919ce45015be0
Mohammed_Elsheikh <moh.reda198@gmail.com>
2023-12-31 23:04:02 +0200

    Add app theme,  app router and splash view.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:atlia_art/core/utils/app_router.dart';
+import 'package:atlia_art/core/utils/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const AtlieArtApp());
@@ -9,6 +12,13 @@ class AtlieArtApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp.router(
+      routerConfig: AppRouter.router,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(
+        scaffoldBackgroundColor: kThairdColor,
+        textTheme: GoogleFonts.interTextTheme(),
+      ),
+    );
   }
 }
