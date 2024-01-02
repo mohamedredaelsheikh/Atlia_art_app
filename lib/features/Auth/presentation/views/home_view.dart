0981fa0c5c99395cc7137e244c14404166adcbd0
Mohammed_Elsheikh <moh.reda198@gmail.com>
2024-01-02 12:55:45 +0200

    create home view.

---
+++ lib/features/Auth/presentation/views/home_view.dart
@@ -0,0 +1,13 @@
+import 'package:atlia_art/features/Auth/presentation/widgets/home_view_body.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: HomeViewBody(),
+    );
+  }
+}
